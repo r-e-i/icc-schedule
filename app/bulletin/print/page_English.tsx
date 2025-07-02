795906243217fc263e079dc8706f146a88c47f29
Rei Suryana <rsuryana@ucmerced.edu>
2025-07-01 20:48:41 -0700

    Update bulletin pages with Indonesian translations and improve layout

--- app/bulletin/print/page_English.tsx
+++ app/bulletin/print/page_English.tsx
@@ -52,7 +52,7 @@ const Bulletin: React.FC = () => {
                         ))}
                      { NumberOfAnnouncements > 3 && 
                         <div className="w-full px-3 mt-5 ">
-                           <div className="font-bold">P E N G U M U M A N <small><i>Dari Halaman Sebelumnya</i></small></div>
+                           <div className="font-bold">A N N O U N C E M E N T <small><i>Continued from previous page</i></small></div>
                            <div className='px-4'> 
                             <Announcement schedule={CurrentSundaySchedule} indexstart={4} indexend={NumberOfAnnouncements} />
                             </div>
@@ -61,11 +61,11 @@ const Bulletin: React.FC = () => {
                     </div>
                         <div className="w-full px-3">
                             <div className="w-full h-[120px] bg-gray-500 text-white object-cover z-10 rounded-lg shadow-md shadow-gray-500">
-                                <div className='text-center text-xl tracking-widest bg-white bg-opacity-30'>       P E R S E M B A H A N</div>
+                                <div className='text-center text-xl tracking-widest bg-white bg-opacity-30'>        G I V E</div>
                                 <div className="items-center p-2 text-center text-sm">
-                                    Jemaat bisa memberikan persembahan: <br>
-                                    </br>1. Menulis Drop Box yang sudah tersedia. 
-                                    <br></br>2. Menulis cek yang ditunjukan kepada : <b>Immanuel Community Church in Fresno.</b>. Mohon ditulis secara lengkap. 
+                                    There are two ways to give to Immanuel Community Church. You can send your check to ICC Treasurer and/or by giving today by using the Drop Box in the lobby.
+                                    <br></br>Please write in full to: <b>Immanuel Community Church in Fresno.</b>
+
                                 </div>
                             </div>
                         </div>
@@ -77,19 +77,19 @@ const Bulletin: React.FC = () => {
                             <img className="h-[240px] object-cover z-20 " src="/images/Welcome ICC.png" alt="random" />
                         </div>
                         <img className="w-full h-[240px] object-cover z-10 rounded-lg shadow-lg shadow-gray-500 -mb-6" src={CurrentSundaySchedule["PICTURE"]} alt="MISSING PICTURE - please add picture address / URL to picture column on google sheet" />
-                        <div className='text-center font-bold text-2xl tracking-widest text-white z-40 drop-shadow-xl'>        I N D O N E S I A N &nbsp; S E R V I C E</div>
+                        <div className='text-center font-bold text-2xl tracking-widest text-white z-40 drop-shadow-xl'>        S U N D A Y &nbsp; S E R V I C E</div>
 
-                        <div className="text-center tracking-widest mt-2">THEMA KHOTBAH </div>
+                        <div className="text-center tracking-widest mt-2">THEMA KHOTBAH / SERMON TITLE</div>
                         <div className="text-center font-bold text-xl">{CurrentSundaySchedule["SERMON_TITLE_IND"]}</div>
                         <div className="text-center italic text-xl -mt-1">{CurrentSundaySchedule["SERMON_TITLE_ENG"]}</div>
-                        <div className="text-center tracking-widest mt-2">PEMBACAAN ALKITAB  </div>
+                        <div className="text-center tracking-widest mt-2">PEMBACAAN ALKITAB / BIBLE READING </div>
                         <div className="text-center font-bold text-xl -mt-1">{CurrentSundaySchedule["VERSE"]}</div>
 
-                        <div className="text-center tracking-widest mt-2">PEMBICARA  </div>
+                        <div className="text-center tracking-widest mt-2">PEMBICARA / SPEAKER </div>
                         <div className="text-center font-bold text-xl -mt-1">{CurrentSundaySchedule["SPEAKER"]}</div>
 
                         <div className="w-full h-[120px] bg-yellow-900 text-white object-cover z-10 rounded-lg shadow-md shadow-gray-500 mt-2">
-                            <div className='text-center text-xl tracking-widest bg-white bg-opacity-30 p-1'>        H U B U N G I  &nbsp; K A M I</div>
+                            <div className='text-center text-xl tracking-widest bg-white bg-opacity-30 p-1'>        C O N N E C T  &nbsp; W I T H  &nbsp; U S</div>
                             <div className="flex justify-between items-center p-2">
                                 <div className='flex text-sm font-bold'>4141 N MILLBROOK AVE<br />FRESNO, CA 93726<br />(559)500-8220</div>
                                 <div className='text-center text-sm font-bold '>http://iccfresno.org</div>
@@ -108,26 +108,26 @@ const Bulletin: React.FC = () => {
                             <img className="h-[70px] object-cover -mb-1" src="/images/Worship.png" />
                         </div>
                         <div className="flex flex-col text-center space-y-0.5 text-sm">
-                            {wT("Doa Teduh", "DUDUK")}
-                            {wT("Panggilan Beribadah", "BERDIRI")}
+                            {wT("Solemn Prayer", "SIT")}
+                            {wT("Call to Worship", "STAND")}
                             <div className='px-1'>
                                 <b className="-mt-2">{CurrentSundaySchedule["WORSHIP_VERSE"]}</b>
                                 <p className="text-xs italic mx-10 -mt-1 mb-2">{CurrentSundaySchedule["WORSHIP_VERSE_TEXT"]}</p>
                             </div>
-                            {wT("Lagu Pujian", "BERDIRI")}
-                            {wT("Doa Pembukaan", "BERDIRI")}
-                            {wT("Lagu Pujian", "DUDUK")}
-                            {wT(CurrentSundaySchedule["READING"], "BERDIRI")}
-                            <i>Anak anak sekolah minggu dipersilakan masuk ke kelasnya masing masing</i>
-                            {wT("Bible Reading", "BERDIRI")}
+                            {wT("Hymn in Praise", "STAND")}
+                            {wT("Opening Prayer", "STAND")}
+                            {wT("Hymn in Praise", "STAND/SIT")}
+                            {wT(CurrentSundaySchedule["READING"], "STAND")}
+                            <i>Sunday School may go into the classes</i>
+                            {wT("Bible Reading", "STAND")}
                             <div className="font-bold -mb-3">{CurrentSundaySchedule["VERSE"]}</div>
                             <p className="italic mx-10 -mt-1">{CurrentSundaySchedule["SERMON_TITLE_IND"]}</p>
-                            {wT("Lagu Pujian", "BERDIRI")}
-                            {wT("Khotbah", "SIT")}
-                            {wT("Lagu Doksologi", "BERDIRI")}
-                            {wT("Doa Berkat", "BERDIRI")}
-                            {wT("Lagu Amin", "BERDIRI")}
-                            {wT("Doa Teduh", "DUDUK")}
+                            {wT("Hymn in Praise", "STAND")}
+                            {wT("Sermon", "SIT")}
+                            {wT("Doxology", "STAND")}
+                            {wT("Benediction", "STAND")}
+                            {wT("Amen", "STAND")}
+                            {wT("Solemn Prayer", "STAND")}
                         </div>
                         <div className="flex justify-center">
                             <img className="h-[70px] object-cover" src="/images/Announcement.png" />
@@ -141,7 +141,7 @@ const Bulletin: React.FC = () => {
                     {/* Second Column */}
                     <div className="w-1/2 p-1">
                         <div className="w-full bg-yellow-700 text-white object-cover z-10 rounded-lg m-1 ">
-                            <div className='text-center text-lg tracking-widest bg-white bg-opacity-30'>  <b> IBADAH PAGI INI </b> | <small>{thisSunday.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }).toUpperCase()} at 10:30 AM</small></div>
+                            <div className='text-center text-lg tracking-widest bg-white bg-opacity-30'>  <b> TODAY&apos;S SERVICE </b> | <small>{thisSunday.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }).toUpperCase()} at 10:30 AM</small></div>
                         </div>
                         <table className="w-full mx-3 mt-2">
                             <tbody>
@@ -149,7 +149,7 @@ const Bulletin: React.FC = () => {
                             </tbody>
                         </table>
                         <div className="w-full bg-yellow-800 text-white object-cover z-10 rounded-lg m-1 ">
-                            <div className='text-center text-xl tracking-widest bg-white bg-opacity-30'>  <b> AKTIFITAS </b> </div>
+                            <div className='text-center text-xl tracking-widest bg-white bg-opacity-30'>  <b> ACTIVITIES </b> </div>
                         </div>
                         <table className="w-full mx-3 mt-2">
                             <tbody>
@@ -157,7 +157,7 @@ const Bulletin: React.FC = () => {
                             </tbody>
                         </table>
                         <div className="w-full bg-yellow-900 text-white object-cover z-10 rounded-lg m-1 ">
-                            <div className='text-center text-lg tracking-widest bg-white bg-opacity-30'>  <b> IBADAH MINGGU DEPAN</b> | <small>{nextSunday.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }).toUpperCase()} at 10:30 AM</small></div>
+                            <div className='text-center text-lg tracking-widest bg-white bg-opacity-30'>  <b> SUNDAY SERVICE </b> | <small>{nextSunday.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }).toUpperCase()} at 10:30 AM</small></div>
                         </div>
                         <table className="w-full mx-3 mt-2">
                             <tbody>
